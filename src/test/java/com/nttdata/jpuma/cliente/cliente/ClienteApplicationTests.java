4955c7283d0a1ea0fb6b833b56dea7fd35494597
Javier Serapio Pumaccajia Tasilla <jpumacca@emeal.nttdata.com>
2023-07-07 12:57:10 -0500

    Inicio de Proyecto

---
+++ src/test/java/com/nttdata/jpuma/cliente/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.jpuma.cliente.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
